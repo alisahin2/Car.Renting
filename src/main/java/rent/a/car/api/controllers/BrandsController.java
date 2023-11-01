07187d2fd078b8cc03a1dc74c30c349f5e3aa686
ali <alisahin2607@gmail.com>
2023-11-01 15:03:57 +0300

    All Brand layers created and filled

---
+++ src/main/java/rent/a/car/api/controllers/BrandsController.java
@@ -0,0 +1,25 @@
+package rent.a.car.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rent.a.car.business.abstracts.BrandService;
+import rent.a.car.entities.concretes.Brand;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/brands")
+public class BrandsController {
+
+    private BrandService brandService;
+
+    public BrandsController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @GetMapping("/getAll")
+    public List<Brand> getAll(){
+        return brandService.getAll();
+    }
+}
